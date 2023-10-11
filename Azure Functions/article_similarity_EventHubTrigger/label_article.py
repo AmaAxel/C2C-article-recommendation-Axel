69165207a061c12e01fbfdfc9d55ff87ec1af1ef
Francois <chabannes.francois@gmail.com>
2023-10-11 15:16:43 +0100

    work around tagging API failing

--- Azure Functions/article_similarity_EventHubTrigger/label_article.py
+++ Azure Functions/article_similarity_EventHubTrigger/label_article.py
@@ -14,44 +14,50 @@ async def get_best_labels(properties):
 
 async def get_labels(text):
 
-    headers = {
-        'Authorization': 'Bearer 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',
-        'Content-Type': 'application/json'
-    }
+    # the API is sometime down so in case the API call fails, we give a equal 1 score to all categories
+    try:
+        headers = {
+            'Authorization': 'Bearer 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',
+            'Content-Type': 'application/json'
+        }
 
-    data = '{"inputs": "%s"}' % text[:512].replace('"', '\\"').replace("'", "\\'").encode('utf-8', 'replace')
-    url = 'https://api-inference.huggingface.co/models/abhishek/autonlp-bbc-news-classification-37229289'
-
-    response = requests.post(url, headers=headers, data=data)
-
-    logging.info('**RESPONSE: %s', response.text)
-
-    t = 0
-    while True and t < 10:
+        data = '{"inputs": "%s"}' % text[:512].replace('"', '\\"').replace("'", "\\'").encode('utf-8', 'replace')
+        url = 'https://api-inference.huggingface.co/models/abhishek/autonlp-bbc-news-classification-37229289'
 
         response = requests.post(url, headers=headers, data=data)
-        
-        if response.ok:
-            # Extract the list of labels and scores from the predictions
-            label_scores = response.json()[0]
-            
-            # Sort the labels based on the scores
-            sorted_labels = sorted(label_scores, key=lambda x: x['score'], reverse=True)
-            
-            # Create a dictionary with the ranking of each label
-            rankings = {}
-            for i, label in enumerate(sorted_labels):
-                rankings[label['label']] = i + 1
 
-            return rankings
-        
-        elif "currently loading" in response.text:
-            estimated_time = response.json()["estimated_time"]
-            logging.info("Model is currently loading, waiting for %.2f seconds before retrying", 8)
-            await asyncio.sleep(8)            
-            t = t + 1
-        else:
-            raise ValueError(f"Failed to get response from Hugging Face API: {response.text}")
+        logging.info('**RESPONSE: %s', response.text)
 
-    logging.info('**Error in model-tagging API call: %s', response.text)
-    raise ValueError('Could not get answer model-tagging API: %s', response.text)
+        t = 0
+        while True and t < 10:
+
+            response = requests.post(url, headers=headers, data=data)
+            
+            if response.ok:
+                # Extract the list of labels and scores from the predictions
+                label_scores = response.json()[0]
+                
+                # Sort the labels based on the scores
+                sorted_labels = sorted(label_scores, key=lambda x: x['score'], reverse=True)
+                
+                # Create a dictionary with the ranking of each label
+                rankings = {}
+                for i, label in enumerate(sorted_labels):
+                    rankings[label['label']] = i + 1
+
+                return rankings
+            
+            elif "currently loading" in response.text:
+                estimated_time = response.json()["estimated_time"]
+                logging.info("Model is currently loading, waiting for %.2f seconds before retrying", 8)
+                await asyncio.sleep(8)            
+                t = t + 1
+            else:
+                raise ValueError(f"Failed to get response from Hugging Face API: {response.text}")
+
+        logging.info('**Error in model-tagging API call: %s', response.text)
+        raise ValueError('Could not get answer model-tagging API: %s', response.text)
+    
+    except:
+        rankings = {'business': 1, 'politics': 1, 'sport': 1, 'entertainment': 1, 'tech': 1}
+        return rankings
