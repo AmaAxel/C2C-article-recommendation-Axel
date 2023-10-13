661cbaeb507e143f8679b0afd1436940a54c1818
Francois <chabannes.francois@gmail.com>
2023-10-13 13:17:32 +0100

    added first dummy test

---
+++ Azure Functions/tests/test_article_generation.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestMyFunction(unittest.TestCase):
+    def test_dummy_example(self):
+        # This is a dummy test that always passes
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
+
