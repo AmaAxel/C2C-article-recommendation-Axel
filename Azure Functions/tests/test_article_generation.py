e6ea9cc10d8893534509c7f3f86dd39e98a42aaa
Francois <chabannes.francois@gmail.com>
2023-10-22 14:20:50 +0100

    updated test for article generation

--- Azure Functions/tests/test_article_generation.py
+++ Azure Functions/tests/test_article_generation.py
@@ -6,8 +6,7 @@ from newsapi.newsapi_client import NewsApiClient
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from article_generation_TimerTrigger.clean_article import clean_content
-from article_generation_TimerTrigger.extract_content import extract_to_dict
+from article_generation_TimerTrigger.bbc_scraper import BBCArticleScraper
 
 
 
@@ -19,31 +18,23 @@ class TestExtractedArticle(unittest.TestCase):
     """
     def test_extracted_article_structure(self):
 
-        newsapi_key = '6fdb9fb9f5154d1c8073d732a744fb9f'
-        # Search for articles using the everything endpoint
-        newsapi = NewsApiClient(api_key=newsapi_key) 
-
-        # Search for articles using the everything endpoint
-        articles = newsapi.get_everything(sources='bbc-news')
+        bbc_scraper = BBCArticleScraper()
+        articles = bbc_scraper.scrape_bbc_articles()
 
         # Retrieve the full content of each article using the urlToImage field
-        for article in articles['articles']:
-            article = extract_to_dict(article)
+        for article in articles:
 
             # Check if article is a dictionary
             self.assertIsInstance(article, dict)
 
             # Check if article has the expected keys
-            expected_keys = ['title', 'description', 'publishedAt', 'content']
+            expected_keys = ['title', 'publishedAt', 'section', 'URL', 'description', 'content']
             for key in expected_keys:
                 self.assertIn(key, article)
 
             # Check if none of the values are empty strings
             for key, value in article.items():
                 self.assertNotEqual(value, '', f"Value for '{key}' is an empty string.")
-
-            break
-
 
 if __name__ == '__main__':
     unittest.main()
